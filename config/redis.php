f03519fc3bdbd569bf1458d906b8631d81a18501
JR <jrazar@hotmail.com>
2026-05-15 19:36:06 -0300

    define base redis por defecto

--- config/redis.php
+++ config/redis.php
@@ -3,7 +3,8 @@
 function getRedis() {
     try {
         $redis = new Redis();
-        $redis->connect('127.0.0.1', 6379);          
+        $redis->connect('127.0.0.1', 6379); 
+        $redis->select(0);         
         return $redis;
     } catch (Throwable $e) {
         return null;
